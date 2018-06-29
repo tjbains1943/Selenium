f2b6196a2596542fa7eaa2ddf026b643b9cc509a
tj Bains <tjbains1943@yahoo.com>
2018-06-28 19:33:06 -0700

    commit changes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require("../models/users");
 
 
 //Mongoose query abstractions
--- routes/mongoApi/users.js
+++ routes/mongoApi/users.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
 const userControllers = require("../../controllers/userControllers");
 
-router.route("/")
-  .get(userControllers.findLogin)
-  .post(userControllers.create);
+router
+  .route("/")
+  .post(userControllers.findLogin)
+  .get(userControllers.create);
 
 router
   .route("/:id")
   .get(userControllers.findById)
   .put(userControllers.update)
   .delete(userControllers.remove);
-
-  
 
 module.exports = router;
