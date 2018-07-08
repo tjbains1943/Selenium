28713d2bff9e41df3d686839b237dacf403917e8
tj Bains <tjbains1943@yahoo.com>
2018-07-08 13:19:30 -0700

    display data

--- client/src/pages/dailyTrainer/dailyTrainer.js
+++ client/src/pages/dailyTrainer/dailyTrainer.js
@@ -36,39 +36,39 @@ class DailyTrainer extends Component {
 //    }
 
   render() {
-    // if (this.state.results) {
-    //     return (
-    //         <div className="text-white">
-    //         <Nav />
-    //         <h5 className="text-white">
-    //             Please select the Daily Trainer to be viewed.
-    //           </h5>
-    //           <select name="trainerType" onChange={this.handleInputChange} required>
-    //           <option style={{"display":"none"}} disabled selected value> -- select an option -- </option>
-    //             <option value="Mens">Men's</option>
-    //             <option value="Womens">Women's</option>
-    //             <option value="30-min">30-min</option>
-    //             <option value="Beginner">Beginner</option>
-    //           </select>
-    //           <button onClick={this.saveUser} className="btn-danger py-1 px-5 mt-3 d-block">Submit</button>
-    //           {this.state.results.data.map((x, numb) => (
-    //                 <div key={numb} className="text-white"> {x.day}
+    if (this.state.results) {
+        return (
+            <div className="text-white">
+            <Nav />
+            <h5 className="text-white">
+                Please select the Daily Trainer to be viewed.
+              </h5>
+              <select name="trainerType" onChange={this.handleInputChange} required>
+              <option style={{"display":"none"}} disabled selected value> -- select an option -- </option>
+                <option value="Mens">Men's</option>
+                <option value="Womens">Women's</option>
+                <option value="30-min">30-min</option>
+                <option value="Beginner">Beginner</option>
+              </select>
+              <button onClick={this.saveUser} className="btn-danger py-1 px-5 mt-3 d-block">Submit</button>
+              {this.state.results.data.map((x, numb) => (
+                    <div key={numb} className="text-white"> {x.day}
 
-    //                   {x.exercise.map((y, num) => (
-    //                     <div key={num}> {`Exercise: ${num}    Name: ${y.name}  Weight:${y.Weight}    Sets:${y.Sets}  Reps:${y.Reps} time between:${y.time}`}</div>
+                      {x.exercise.map((y, num) => (
+                        <div key={num}> {`Exercise: ${num}    Name: ${y.name}  Weight:${y.Weight}    Sets:${y.Sets}  Reps:${y.Reps} time between:${y.time}`}</div>
                 
-    //                   )
-    //                 )
-    //                   }
+                      )
+                    )
+                      }
                       
-    //                   </div>
+                      </div>
                       
-    //               )
-    //             )
-    //             }
-    //         </div>
-    //     );
-    //   }
+                  )
+                )
+                }
+            </div>
+        );
+      }
     return (
       <div className="text-white">
         <Nav />
