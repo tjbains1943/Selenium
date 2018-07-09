890cc3b9eee18972448ab9436b595c2f69930864
Kyle Thomas <k.thomas04@yahoo.com>
2018-07-09 14:24:42 -0700

    style updates

--- client/src/pages/Daily/daily.js
+++ client/src/pages/Daily/daily.js
@@ -39,8 +39,17 @@ class Daily extends Component {
           <div id="backgorund" className="col-md-6">
             <p id="script">-The daily trainer is the ideal program for the every day gym goer. If you've ever gotten tired of your routine or just flat out need more workout material then the daily trainer is where you need to be!</p> <p  id="script">-Fully customized workouts for every day of the week. The workouts are updated every week and dont be discouraged if you find some of the workouts too hard.</p><p  id="script">-We have beginner, advanced and 30 minute express workouts for those who don't always have time to be at the gym for very long. </p>
             
+            </div>
+            
+          <div className="col-md-6">
+          <img id="dailypic" src="http://anytime-deals.com/wp-content/uploads/2018/02/pexels-photo-414029.jpg" alt="First slide"></img>
+          
+          </div>
 
-            <h3 id="DT">-MENS DAILY TRAINER</h3>
+      </div>
+      <div className="row">
+      <div className="col-md-6">
+      <h3 id="DT">-MENS DAILY TRAINER</h3>
               {/* <table className="table table-bordered">
                 <thead>
                   <tr>
@@ -56,6 +65,7 @@ class Daily extends Component {
                 <tbody className="text-white"> */}
 
                   {/* {this.state.results.data[0].exercise[0].id} */}
+                  
                   {this.state.results.data.map((x, numb) => (
                     <div key={numb} className="text-white"> {x.day}
 
@@ -74,15 +84,45 @@ class Daily extends Component {
                 {/* </tbody>
       
               </table> */}
-            <h3 id="DT">-WOMENS DAILY TRAINER</h3>
-            
-            </div>
-            
+              </div>
           <div className="col-md-6">
-          <img id="dailypic" src="http://anytime-deals.com/wp-content/uploads/2018/02/pexels-photo-414029.jpg" alt="First slide"></img>
-          
-          </div>
+            <h3 id="DT">-WOMENS DAILY TRAINER</h3>
+            {/* <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th className="text-white">Exercise</th>
+                    <th className="text-white">Weight</th>
+                    <th className="text-white">Reps</th>
+                    <th className="text-white">Sets</th>
+                    <th className="text-white">Time between Sets(optional)</th>
 
+                  </tr>
+                </thead>
+      
+                <tbody className="text-white"> */}
+
+                  {/* {this.state.results.data[0].exercise[0].id} */}
+                  
+                  {this.state.results.data.map((x, numb) => (
+                    <div key={numb} className="text-white"> {x.day}
+
+                      {x.exercise.map((y, num) => (
+                        <div key={num}> {`Exercise: ${num}    Name: ${y.name}  Weight:${y.Weight}    Sets:${y.Sets}  Reps:${y.Reps} time between:${y.time}`}</div>
+                
+                      )
+                    )
+                      }
+                      
+                      </div>
+                      
+                  )
+                )
+                }
+                {/* </tbody>
+      
+              </table> */}
+            </div>
+      
       </div>
       <div className="col-md-12">
           <h4 id="footer">Stop sitting on the sidelines! Become the version of you that you always wanted!</h4>
