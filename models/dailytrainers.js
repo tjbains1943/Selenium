be62a454131efe04ce6945ca1b3d5fff3f137657
tj Bains <tjbains1943@yahoo.com>
2018-07-10 19:49:49 -0700

    add chagne

--- models/dailytrainers.js
+++ models/dailytrainers.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const DailyTrainer = new Schema({
+const dailyTrainers = new Schema({
     trainerType: {type: String, required: false},
     weekStart: { type: String, required: false },
     day: {type: String},
     exercise: { type: [{}], required: false},
 });
 
-const DailyTrainers = mongoose.model("DailyTrainers", DailyTrainer);
+const DailyTrainers = mongoose.model("DailyTrainers", dailyTrainers);
 
 module.exports = DailyTrainers;
