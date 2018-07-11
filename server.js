3f87359b2140f30f19a4dd26639ad0ac2c6b0d0d
tj Bains <tjbains1943@yahoo.com>
2018-07-10 19:53:02 -0700

    add change

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.Promise = global.Promise;
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/simplyFIT");
+
 
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,14 +14,16 @@ app.use(bodyParser.json());
 // set up logger
 app.use(logger('dev'))
 // serve up static assets
-if (process.env.NODE_ENV === "production") {
+
 app.use(express.static("client/build"));
-}
+
 // set up routes
 app.use(routes);
 
 // Set up promises with mongoose
-
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/simplyFIT");
 
 // Start the API server
 app.listen(PORT, function() {
