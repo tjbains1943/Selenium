20d3135264c18453f08ffe0e4737831116699576
tj Bains <tjbains1943@yahoo.com>
2018-07-12 15:53:44 -0700

    removed comments

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -41,15 +41,12 @@ module.exports = {
   },
   findById: function(req, res) {
     db.Users.find({})
-    // Specify that we want to populate the retrieved users with any associated notes
     .populate("workouts")
     .then(function(dbUser) {
       console.log(dbUser);
-      // If able to successfully find and associate all Users and Notes, send them back to the client
       res.json(dbUser);
     })
     .catch(function(err) {
-      // If an error occurs, send it back to the client
       res.json(err);
     });
   },
--- controllers/userWorkoutsControllers.js
+++ controllers/userWorkoutsControllers.js
@@ -1,6 +1,5 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
     db.UserWorkouts
@@ -19,17 +18,14 @@ module.exports = {
     db.UserWorkouts
       .create(req.body)
       .then(function(dbModel) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        
         return db.Users.findOneAndUpdate({}, { $push: { workOuts: dbModel._id } }, { new: true })
       })
       .then(function(dbUsers) {
-        // If we were able to successfully update an Article, send it back to the client
+       
         res.json(dbUsers);
       })
       .catch(function(err) {
-        // If an error occurred, send it to the client
         res.json(err);
       });
   
