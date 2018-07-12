bc6088856364f60b817c75a0f6c8b389133257a1
tj Bains <tjbains1943@yahoo.com>
2018-07-12 15:55:33 -0700

    took out required for models

--- models/users.js
+++ models/users.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const users = new Schema({
-    firstName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    firstName: { type: String, required: false },
+    email: { type: String, required: false },
+    password: { type: String, required: false },
     height: { type: String, required: false },
     weight: { type: String, required: false },
     date: { type: Date, required: false },
