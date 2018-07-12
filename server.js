6740ef1d56761ef17dcb89d09f3a5b5649ecbd92
Leo Q <leoq91@gmail.com>
2018-07-11 18:53:07 -0700

    heroku push

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const logger = require('morgan');
+const logger = require("morgan");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // set up logger
-app.use(logger('dev'))
+app.use(logger("dev"));
 // serve up static assets
 
 app.use(express.static("client/build"));
@@ -23,9 +21,11 @@ app.use(routes);
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/simplyFIT");
+mongoose.connect(
+    "mongodb://heroku_9tn1sb2j:no3vq295886njsr1t4balt7eu6@ds133621.mlab.com:33621/heroku_9tn1sb2j"
+);
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
